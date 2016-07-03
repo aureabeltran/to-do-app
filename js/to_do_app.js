189c434a27e97dc17d3797b4a32970c6440c2f3b
Aurea Beltran <aureabeltranmanzo@gmail.com>
2016-07-03 16:44:14 -0300

    arreglo responsive

--- js/to_do_app.js
+++ js/to_do_app.js
@@ -1,18 +1,5 @@
-/*var boton, lista;
-function addEvent () {
-	boton = document.getElementById("ADD");
-	ADD.addEventListener("click", function(){
-		if (li.innerHTML = ""){
-			alert("ingrese texto valido");
-		} 
-		else{
-			lista = document.getElementById("events");
-			item = document.createElement("li");
-		}
-	});
-}
-*/
-var boton, lista, tarea, item, radio ;
+
+var boton, lista, tarea, item, text, check, trash, borrar, basurero;
 //aqui tomo el boton de id ADD
 boton = document.getElementById("ADD");
 //le asigno el click y una funcion para eso con un evento
@@ -21,20 +8,61 @@ boton.addEventListener("click",function(){
 	tarea = document.getElementById("events");
 //asigno a mi variable lista que haya creado un li
 	lista = document.createElement("li");
-//agregar input
-	radio = document.createElement("input");
-	radio.setAttribute("class", "option");
+	lista.setAttribute("id", "listado");
 //con item tomo el valor ingresado en mi text area
-	item = document.getElementById("texto").value;
+	text = document.getElementById("texto");
+	item = text.value;
+//si el largo del texto supera los 0 caracteres, entonces grega la tarea
 	if (item.length > 0) {
-	//var t guarda lo q se ingreso en el textarea
+//var t guarda lo q se ingreso en el textarea
 		var t = document.createTextNode(item);
-	//le digo a lista(li) que guarde t
-		lista.appendChild(t, radio);
-	//luego guardo lista(li) dentro de mi "tarea"(ul)
+//agregar input
+		text.value = "";
+		check = document.createElement("input");
+		check.setAttribute("class", "option");
+		check.setAttribute("type", "checkbox");
+		check.setAttribute("id", "check");
+		check.setAttribute("onclick", "marcar()");
+		caja = document.createElement("button");
+		caja.setAttribute("id", "basurero");
+		caja.setAttribute("onclick", "eliminar()");
+		trash = document.createElement("i");
+		trash.setAttribute("class", "fa fa-trash");
+		caja.appendChild(trash);
+//le digo a lista(li) que guarde las variables una por una
+		lista.appendChild(check);
+		lista.appendChild(t);
+		lista.appendChild(caja);
+//luego guardo lista(li) dentro de mi "tarea"(ul)
 		tarea.appendChild(lista);	
 	}
+//si no, lanza una alerta para que ingrese texto
 	else{
-		alert("Ingrese Tarea");
+		alert("Add a Valid Event");
 	}
 })
+
+//tachar tarea
+function marcar () {
+	check = document.getElementById("check")
+	if (check.checked == true){
+		listado = document.getElementById("listado");
+		listado.classList.add("tachado");
+		console.log(listado);
+	}
+	else{
+		listado = document.getElementById("listado");
+		listado.classList.remove("tachado");
+		console.log(listado);
+	}
+}
+
+//borrar Tarea
+
+function eliminar () {
+	var listado = document.getElementById("events");
+	var list = listado.firstChild;
+	console.log(list);
+	listado.removeChild(list);
+}
+	
